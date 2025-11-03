b0b5536e649ec6e9406fbeeeaca9e0238b54bfc1
Frank <fha@easv.dk>
2025-11-03 18:55:20 +0100

    addad a calculator

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+
+    /**
+     *Adds magic numbers
+     * @param summant1
+     * @param sumant2
+     * @return
+     */
+    public static int addNumbers(int summant1, int sumant2){return summant1+sumant2;}
+}
